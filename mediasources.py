9de4dd4aa937c54df5442fec36e8e8c3c662d2ad
Alexandre Goncalves <alexandre.reportagem@gmail.com>
2014-10-20 10:42:23 -0400

    download all data

--- mediasources.py
+++ mediasources.py
@@ -18,6 +18,45 @@ with con:
     cur.execute('SET character_set_connection=utf8;')
 
     stopIteration = False
+    lastProcessed_tag_sets_id = 0
+
+    while not stopIteration:
+        tagSets = mc.tagSetList(last_tag_sets_id=lastProcessed_tag_sets_id)
+        if len(tagSets) == 0:
+            stopIteration = True
+        else:
+            print "============================"
+            print "More records: " + str(len(tagSets))
+            print "============================"
+            for tagSet in tagSets:
+                print tagSet['name']
+                cur.execute('INSERT INTO tag_set '
+                            '(tag_sets_id, name, label, description, show_on_media, show_on_stories)'
+                            'values (%s, %s, %s, %s, %s, %s)',
+                            (tagSet['tag_sets_id'], tagSet['name'], tagSet['label'], tagSet['description'],
+                            tagSet['show_on_media'], tagSet['show_on_stories']))
+                lastProcessed_tag_sets_id = tagSet['tag_sets_id']
+
+                stopNestedIteration = False
+                lastProcessed_tags_id = 0
+                while not stopNestedIteration:
+                    tags = mc.tagList(tag_sets_id=tagSet['tag_sets_id'], last_tags_id=lastProcessed_tags_id)
+                    if len(tags) == 0:
+                        stopNestedIteration = True
+                    else:
+                        print "============================"
+                        print "More records: " + str(len(tags))
+                        print "============================"
+                        for tag in tags:
+                            print tag['tag']
+                            cur.execute('INSERT INTO tag '
+                                        '(tags_id, tag_sets_id, tag, label, description, show_on_media, show_on_stories)'
+                                        'values (%s, %s, %s, %s, %s, %s, %s)',
+                                        (tag['tags_id'], tag['tag_sets_id'], tag['tag'], tag['label'], tag['description'],
+                                        tag['show_on_media'], tag['show_on_stories']))
+                            lastProcessed_tags_id = tag['tags_id']
+
+    stopIteration = False
     lastProcessed_media_sets_id = 0
 
     while not stopIteration:
@@ -35,3 +74,37 @@ with con:
                             'values (%s, %s, %s)',
                             (mediaSet['media_sets_id'], mediaSet['name'], mediaSet['description']))
                 lastProcessed_media_sets_id = mediaSet['media_sets_id']
+
+    stopIteration = False
+    lastProcessed_media_id = 0
+
+    while not stopIteration:
+        media = mc.mediaList(lastProcessed_media_id, 50000)
+        if len(media) == 0:
+            stopIteration = True
+        else:
+            print "============================"
+            print "More records: " + str(len(media))
+            print "============================"
+            for medium in media:
+                print medium['url']
+                lastProcessed_media_id = medium['media_id']
+                if len(medium['url']) <= 500:
+                    cur.execute('INSERT INTO media '
+                                '(media_id, url, name)'
+                                'values (%s, %s, %s)',
+                                (medium['media_id'], medium['url'], medium['name']))
+                    if 'media_sets' in medium.keys():
+                        for media_set_aux in medium['media_sets']:
+                            print '...' + str(media_set_aux['media_sets_id'])
+                            cur.execute("INSERT into media_media_set "
+                                        "(media_id, media_sets_id) "
+                                        "values (%s, %s)",
+                                        ( medium['media_id'], media_set_aux['media_sets_id']))
+                    if 'media_source_tags' in medium.keys():
+                        for tag_aux in medium['media_source_tags']:
+                            print '...' + str(tag_aux['tags_id'])
+                            cur.execute("INSERT into media_tag "
+                                        "(media_id, tags_id) "
+                                        "values (%s, %s)",
+                                        ( medium['media_id'], tag_aux['tags_id']))
--- mysql.sql
+++ mysql.sql
@@ -1,30 +1,132 @@
-CREATE TABLE `test`.`media_set` (
-  `media_sets_id` INT NOT NULL,
-  `name` VARCHAR(100) NULL,
-  `description` VARCHAR(300) NULL,
-  PRIMARY KEY (`media_sets_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `test`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.6.21-log
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `media`
+--
+
+DROP TABLE IF EXISTS `media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `media` (
   `media_id` int(11) NOT NULL,
-  `url` varchar(300) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
+  `url` varchar(500) DEFAULT NULL,
+  `name` varchar(300) DEFAULT NULL,
   PRIMARY KEY (`media_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `test`.`media_media_set` (
-  `media_id` INT NOT NULL,
-  `media_set_id` INT NOT NULL,
-  PRIMARY KEY (`media_id`, `media_set_id`),
-  INDEX `media_sets_id_idx` (`media_set_id` ASC),
-  CONSTRAINT `media_id`
-    FOREIGN KEY (`media_id`)
-    REFERENCES `test`.`media` (`media_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `media_sets_id`
-    FOREIGN KEY (`media_set_id`)
-    REFERENCES `test`.`media_set` (`media_sets_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+--
+-- Table structure for table `media_media_set`
+--
+
+DROP TABLE IF EXISTS `media_media_set`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `media_media_set` (
+  `media_id` int(11) NOT NULL,
+  `media_sets_id` int(11) NOT NULL,
+  PRIMARY KEY (`media_id`,`media_sets_id`),
+  KEY `media_sets_id_idx` (`media_sets_id`),
+  CONSTRAINT `media_id` FOREIGN KEY (`media_id`) REFERENCES `media` (`media_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `media_sets_id` FOREIGN KEY (`media_sets_id`) REFERENCES `media_set` (`media_sets_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `media_set`
+--
+
+DROP TABLE IF EXISTS `media_set`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `media_set` (
+  `media_sets_id` int(11) NOT NULL,
+  `name` varchar(300) DEFAULT NULL,
+  `description` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`media_sets_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `media_tag`
+--
+
+DROP TABLE IF EXISTS `media_tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `media_tag` (
+  `media_id` int(11) NOT NULL,
+  `tags_id` int(11) NOT NULL,
+  PRIMARY KEY (`media_id`,`tags_id`),
+  KEY `tag_id_fk_idx` (`tags_id`),
+  CONSTRAINT `media_id_fk` FOREIGN KEY (`media_id`) REFERENCES `media` (`media_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `tag_id_fk` FOREIGN KEY (`tags_id`) REFERENCES `tag` (`tags_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
+  `tags_id` int(11) NOT NULL,
+  `tag_sets_id` int(11) DEFAULT NULL,
+  `tag` varchar(300) DEFAULT NULL,
+  `label` varchar(300) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `show_on_media` int(11) DEFAULT NULL,
+  `show_on_stories` int(11) DEFAULT NULL,
+  PRIMARY KEY (`tags_id`),
+  KEY `tag_set_idx` (`tag_sets_id`),
+  CONSTRAINT `tag_set` FOREIGN KEY (`tag_sets_id`) REFERENCES `tag_set` (`tag_sets_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tag_set`
+--
+
+DROP TABLE IF EXISTS `tag_set`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag_set` (
+  `tag_sets_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `label` varchar(300) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `show_on_media` int(11) DEFAULT NULL,
+  `show_on_stories` int(11) DEFAULT NULL,
+  PRIMARY KEY (`tag_sets_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-10-20 10:40:37
